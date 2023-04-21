d602fb49ec9c96f36c6dda69c7a3d8a5a714673f
benjamin priour <benjamin.priour@grenoble-inp.org>
2023-04-21 17:42:51 +0200

    fully initialized

--- server/test/test.js
+++ server/test/test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('App', function() {
-  it('has the default page', function(done) {
+describe('App', () => {
+  test('has the default page', () => {
     request(app)
       .get('/')
-      .expect(/Welcome to Express/, done);
+      .expect(/Welcome to Express/);
   });
 }); 
