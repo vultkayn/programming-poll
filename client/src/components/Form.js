48573de3f00dc4c17099acbde57eb5e58a4624f9
benjamin priour <benjamin.priour@grenoble-inp.org>
2023-05-02 01:04:43 +0200

    Corrected Input component and added validation to all inputs of the login/signup forms

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,71 +1,84 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import './styles/Form.css'
-import Debug from 'debug'
-const debug = Debug('component:Form')
+import './styles/Form.css';
+import Debug from 'debug';
+const debug = Debug('component:Form');
 
-export function Input({
-    name,
-    className,
-    label = "",
-    validator = (name, value) => {},
-    type = "text",
-    bubbleUp = false,
-    required = false,
-    onFocus = (e) => {}}) {
-    function handleChange (e) {
-        e.preventDefault();
-        if (! bubbleUp) e.stopPropagation();
-        validator(e.target.name, e.target.value);
-    }
+export function Input ({
+  name,
+  className = '',
+  invalidCls = 'invalid',
+  noInvalidMsg = false,
+  id = '',
+  label = "",
+  validator = (name, value, setMsg) => true,
+  type = "text",
+  bubbleUp = false,
+  required = true,
+  onFocus = (e) => { },
+  extra = {}
+}) {
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [invalidMsg, setInvalidMsg] = useState('Invalid field');
 
-    let input = <input
-        className={`input ${className}`}
-        name={name} 
-        type={type} 
-        onChange={handleChange}
-        onFocus={onFocus}
-        required={required} />
+  function handleChange (e) {
+    e.preventDefault();
+    if (!bubbleUp) e.stopPropagation();
+    setInvalidMsg('Invalid field');
+    setIsInvalid(validator(e.target.name, e.target.value, setInvalidMsg));
+  }
 
-    return (
-        <>
-        {label ? <label htmlFor={name}>input</label> : input}
-        </>
-    );
+
+  let input = <input
+    id={id}
+    className={`input ${className} ${isInvalid ? invalidCls : ''}`}
+    name={name}
+    type={type}
+    onChange={handleChange}
+    onFocus={onFocus}
+    required={required}
+    {...extra} />
+
+  return (
+    <>
+      {label ? <label htmlFor={name}>input</label> : input}
+      {isInvalid && invalidMsg && !noInvalidMsg ? <span className='form-error'>{invalidMsg}</span> : null}
+    </>
+  );
 }
 
-export default function Form({method, endpoint, children, onChange = (e) => {}}) {
-    // const [payload, setPayload] = useState({});
+export default function Form ({ method, endpoint, children, id, onChange = (e) => { } }) {
+  // const [payload, setPayload] = useState({});
 
-    axios.defaults.headers.post['Content-Type'] = 'application/json';
+  axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-    function handleSubmit(e) {
-        e.preventDefault();
+  function handleSubmit (e) {
+    e.preventDefault();
 
-        const form = e.target;
-        const formData = new FormData(form); 
+    const form = e.target;
+    const formData = new FormData(form);
 
-        // FIXME edit API to receive FormData as well.
-        let object = {};
-        formData.forEach((value, key) => object[key] = value);
-        let json = JSON.stringify(object);
+    // FIXME edit API to receive FormData as well.
+    let object = {};
+    formData.forEach((value, key) => object[key] = value);
+    let json = JSON.stringify(object);
 
-        debug("Form submitted to", endpoint);
-        
-        axios({
-            method: method,
-            url: endpoint,
-            data: json
-        })
-        .then((res) => debug(res))
-        .catch((err) => debug("err:", err));
-    }
-    
+    debug("Form submitted to", endpoint);
 
-    return (
-    <form onChange={onChange} onSubmit={handleSubmit}>
-        {children}
+    axios({
+      method: method,
+      url: endpoint,
+      data: json
+    })
+      .then((res) => debug(res))
+      .catch((err) => debug("err:", err));
+  }
+
+
+  return (
+    <form onChange={onChange} onSubmit={handleSubmit} id={id}>
+      {children}
     </form>
-    );
+  );
 }
 
