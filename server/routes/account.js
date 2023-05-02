00315e2e3bdc7f446124cb2549a88ebee4d8447a
benjamin priour <benjamin.priour@grenoble-inp.org>
2023-05-02 16:14:02 +0200

    finalize rebase with master

--- server/routes/account.js
+++ server/routes/account.js
@@ -3,7 +3,6 @@ var router = require('express').Router();
 const { connexion, signup, loggedIn } = require('../passport/authenticate');
 
 router.post('/', signup, (err, req, res, next) => {
-<<<<<<< HEAD
     if (err && err.status == 400)
         if (err.errors !== undefined)
             res.status(err.status).json({errors: err.errors});
@@ -16,17 +15,6 @@ router.post('/login', connexion, (err, req, res, next) => {
             res.status(err.status).json({errors: err.errors});
     
     else next(err.errors || err);
-=======
-  if (err && err.status == 400)
-    res.status(400).json(err.errors);
-  else next(err.errors || err);
-});
-
-router.post('/login', connexion, (err, req, res, next) => {
-  if (err && err.status == 401)
-    res.status(401).json(err.errors);
-  else next(err.errors || err);
->>>>>>> 44e8690 (Added functional router to React. Not all routes have been added already)
 });
 
 
